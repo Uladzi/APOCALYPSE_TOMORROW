56cfa51a9bf0ce24e8cf374e4d1b765273146c1b
Uladzi <bivmak@gmail.com>
2019-11-20 06:14:04 +0300

    feat: get it

---
+++ main.js
@@ -0,0 +1,13 @@
+// Анимация по нажатию навигационной кнопки About us
+document.querySelector(".nav-item:nth-child(2)").addEventListener("click", () => {
+    document.querySelector(".not-anim-font-about-us").classList.add("anim-font-about-us")
+    document.querySelector(".not-anim-font-about-us-p1").classList.add("anim-font-about-us-p1")
+    document.querySelector(".not-anim-font-about-us-p2").classList.add("anim-font-about-us-p2")
+    document.querySelector(".not-anim-font-about-us-p3").classList.add("anim-font-about-us-p3")
+    document.querySelector(".not-anim-font-save-notsave").classList.add("anim-font-save-notsave")
+})
+
+// Анимация по нажатию навигационной кнопки Survival tips
+document.querySelector(".nav-item:nth-child(4)").addEventListener("click", () => {
+    document.querySelector(".not-anim-btns-survival-tips").classList.add("anim-btns-survival-tips")
+})
