bd0a820ebce2cbdd93198628e4b23c62f4f59d03
Uladzi <bivmak@gmail.com>
2019-11-21 08:51:21 +0300

    refactor: make common changes

--- main.js
+++ main.js
@@ -12,10 +12,10 @@ document.querySelector(".nav-item:nth-child(4)").addEventListener("click", () =>
     document.querySelector(".not-anim-btns-survival-tips").classList.add("anim-btns-survival-tips")
 })
 
-// Внезапня картинка :) 
+// // Внезапня картинка :) 
 // const appearSuddenlyPic = () => {
 //     const newImg = document.createElement('img');
-//     newImg.src = 'https://pngimage.net/wp-content/uploads/2018/06/%D0%BA%D1%80%D0%BE%D0%B2%D0%B0%D0%B2%D1%8B%D0%B9-%D0%BE%D1%82%D0%BF%D0%B5%D1%87%D0%B0%D1%82%D0%BE%D0%BA-%D1%80%D1%83%D0%BA%D0%B8-png-1.png'
+//     newImg.src = 'assets/media/wall.jpg'
 //     newImg.id = 'suddenly-pic';
 //     document.querySelector("h1").before(newImg);
 // }
